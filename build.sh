da9887b2b4d55ba5ad97c0271f8401ed05374fd8
mars <dianqi.yu@hotmail.com>
2017-08-29 16:58:49 +0800

    update port from 5000 to 5005

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ function package() {
 
 
 function run() {
-	package && java -Xdebug -Xrunjdwp:transport=dt_socket,address=5000,server=y,suspend=n -jar target/cas-management.war 
+	package && java -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n -jar target/cas-management.war 
 }
 
 if [ $# -eq 0 ]; then
